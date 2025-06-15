252855c2debd699415fa79b11696ec700fa540da
Mohit9674 <mohitnotani97@gmail.com>
2025-06-15 19:52:24 +0100

    added models and templates

--- property_management/properties/views.py
+++ property_management/properties/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Property
 
 class PropertyListView(ListView):
     model = Property
     template_name = 'properties/property_list.html'
     context_object_name = 'properties'
+    
+class PropertyDetailView(DetailView):
+    model = Property
+    template_name = 'properties/property_detail.html'
+    context_object_name = 'property'    
