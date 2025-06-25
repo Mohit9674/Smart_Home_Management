3e4ebb7e77d162bac2b499ea60b200a254bb71b4
Mohit Notani <mohitnotani97@gmail.com>
2025-06-26 00:43:57 +0100

    Update settings.py

--- property_management/property_management/settings.py
+++ property_management/property_management/settings.py
@@ -13,13 +13,13 @@ from django.core.files.storage import default_storage
 from pathlib import Path
 import os
 # Load env
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
+# load_dotenv(BASE_DIR / '.env')
 
 
 # SECURITY
