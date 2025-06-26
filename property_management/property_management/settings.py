bd4eb5caced0237090ac49b0cb8a8bc710ab0a2f
Mohit Notani <mohitnotani97@gmail.com>
2025-06-26 01:11:00 +0100

    Update settings.py

--- property_management/property_management/settings.py
+++ property_management/property_management/settings.py
@@ -13,18 +13,18 @@ from django.core.files.storage import default_storage
 from pathlib import Path
 import os
 # Load env
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# load_dotenv(BASE_DIR / '.env')
+load_dotenv(BASE_DIR / '.env')
 
 
 # SECURITY
 SECRET_KEY   = os.getenv('SECRET_KEY')
-#DEBUG        = os.getenv('DEBUG', 'False') == 'True'
+DEBUG        = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS= os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
@@ -51,8 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'properties',
     'tenants',
-    'cloudinary',
-    'cloudinary_storage',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -167,14 +166,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+# Media via DO Spaces
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_ENDPOINT_URL        = os.getenv("DO_SPACES_ENDPOINT")
+AWS_ACCESS_KEY_ID          = os.getenv("DO_SPACES_KEY")
+AWS_SECRET_ACCESS_KEY      = os.getenv("DO_SPACES_SECRET")
+AWS_S3_REGION_NAME         = os.getenv("DO_SPACES_REGION")
+AWS_STORAGE_BUCKET_NAME    = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_OBJECT_PARAMETERS   = {
+    "CacheControl": "max-age=86400",
 }
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-print("Cloudinary env:", os.getenv('CLOUDINARY_CLOUD_NAME'), os.getenv('CLOUDINARY_API_KEY'), os.getenv('CLOUDINARY_API_SECRET'))
-print("Default file storage:", DEFAULT_FILE_STORAGE)
-
